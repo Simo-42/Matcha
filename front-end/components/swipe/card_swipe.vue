fa430f0fbe7a173cbf746818372ca7c5af41de7d
root <root@Simogoogoo>
2024-09-11 19:37:01 +0200

    Refactorisation de qq partie de code et ajout de socket.io

--- front-end/components/swipe/card_swipe.vue
+++ front-end/components/swipe/card_swipe.vue
@@ -1,122 +1,75 @@
 <template>
-  <div class="w-full max-w-2xl mx-auto mt-10">
-    <!-- Carte et bouton SearchForm -->
-    <div class="relative flex items-center">
-      <!-- Stack de cartes de profils -->
-      <div class="card-stack relative w-3/4">
-        <div
-          v-for="(profile, index) in profiles"
-          :key="profile.id"
-          :class="['card', { 'top-card': index === profiles.length - 1 }]"
-        >
-          <div
-            class="card-content bg-white border border-gray-900 rounded-lg shadow-md overflow-hidden"
-          >
-            <Swiper
-              :slides-per-view="1"
-              :loop="true"
-              :pagination="{ clickable: true }"
-              :initial-slide="0"
-              class="relative w-full aspect-w-16 aspect-h-9"
-            >
-              <!-- Affichage des photos du profil -->
-              <SwiperSlide
-                v-for="(photo, photoIndex) in profile.photos.filter(
-                  (photo) => photo
-                )"
-                :key="photoIndex"
-                class="flex justify-center items-center"
-              >
-                <div class="w-full h-full">
-                  <img
-                    :src="`http://localhost:3005${photo}`"
-                    alt="User Photo"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-              </SwiperSlide>
-            </Swiper>
-            <div class="p-6">
-              <h2 class="text-3xl font-bold text-gray-900">
-                {{ profile.firstname }} {{ profile.lastname }} {{ profile.age }}
-              </h2>
-              <p
-                class="text-gray-600 mt-4 bg-gray-100 p-4 rounded-lg border border-gray-300"
-              >
-                {{ profile.biography }}
-              </p>
-              <div class="mt-4">
-                <div class="flex items-center">
-                  <span class="text-gray-600 font-medium">Gender:</span>
-                  <span class="ml-2 text-gray-800">{{ profile.gender }}</span>
-                </div>
-                <div class="flex items-center mt-2">
-                  <span class="text-gray-600 font-medium">Preference:</span>
-                  <span class="ml-2 text-gray-800">{{
-                    profile.sexual_preference
-                  }}</span>
-                </div>
-                <div class="flex items-center mt-2">
-                  <span class="text-gray-600 font-medium">Location:</span>
-                  <span class="ml-2 text-gray-800">{{ profile.location }}</span>
-                </div>
-              </div>
-              <div class="mt-4">
-                <h3 class="text-lg font-bold text-gray-900">Interests</h3>
-                <div class="flex flex-wrap mt-2">
-                  <span
-                    v-for="(interest, index) in profile.interests"
-                    :key="index"
-                    class="bg-indigo-200 text-indigo-800 rounded-full px-3 py-1 text-sm font-medium mr-2 mb-2"
-                  >
-                    {{ interest }}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+	<div class="w-full max-w-2xl mx-auto mt-10">
+		<!-- Carte et bouton SearchForm -->
+		<div class="relative flex items-center">
+			<!-- Stack de cartes de profils -->
+			<div class="card-stack relative w-3/4">
+				<div v-for="(profile, index) in profiles" :key="profile.id" :class="['card', { 'top-card': index === profiles.length - 1 }]">
+					<div class="card-content bg-white border border-gray-900 rounded-lg shadow-md overflow-hidden">
+						<Swiper :slides-per-view="1" :loop="true" :pagination="{ clickable: true }" :initial-slide="0" class="relative w-full aspect-w-16 aspect-h-9">
+							<!-- Affichage des photos du profil -->
+							<SwiperSlide v-for="(photo, photoIndex) in profile.photos.filter((photo) => photo)" :key="photoIndex" class="flex justify-center items-center">
+								<div class="w-full h-full">
+									<img :src="`http://localhost:3005${photo}`" alt="User Photo" class="w-full h-full object-cover" />
+								</div>
+							</SwiperSlide>
+						</Swiper>
+						<div class="p-6">
+							<h2 class="text-3xl font-bold text-gray-900">{{ profile.firstname }} {{ profile.lastname }} {{ profile.age }}</h2>
+							<p class="text-gray-600 mt-4 bg-gray-100 p-4 rounded-lg border border-gray-300">
+								{{ profile.biography }}
+							</p>
+							<div class="mt-4">
+								<div class="flex items-center">
+									<span class="text-gray-600 font-medium">Gender:</span>
+									<span class="ml-2 text-gray-800">{{ profile.gender }}</span>
+								</div>
+								<div class="flex items-center mt-2">
+									<span class="text-gray-600 font-medium">Preference:</span>
+									<span class="ml-2 text-gray-800">{{ profile.sexual_preference }}</span>
+								</div>
+								<div class="flex items-center mt-2">
+									<span class="text-gray-600 font-medium">Location:</span>
+									<span class="ml-2 text-gray-800">{{ profile.location }}</span>
+								</div>
+							</div>
+							<div class="mt-4">
+								<h3 class="text-lg font-bold text-gray-900">Interests</h3>
+								<div class="flex flex-wrap mt-2">
+									<span v-for="(interest, index) in profile.interests" :key="index" class="bg-indigo-200 text-indigo-800 rounded-full px-3 py-1 text-sm font-medium mr-2 mb-2">
+										{{ interest }}
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        <!-- Boutons like/dislike en bas de la carte -->
-        <div
-          class="tinder--buttons flex justify-around mt-4 w-full max-w-md mx-auto"
-        >
-          <button
-            @click="swipeLeft"
-            class="w-16 h-16 bg-red-500 rounded-full shadow-lg flex items-center justify-center text-2xl text-white"
-          >
-            <i class="fas fa-times"></i>
-          </button>
-          <button
-            @click="swipeRight"
-            class="w-16 h-16 bg-green-500 rounded-full shadow-lg flex items-center justify-center text-2xl text-white"
-          >
-            <i class="fas fa-heart"></i>
-          </button>
-        </div>
-      </div>
+				<!-- Boutons like/dislike en bas de la carte -->
+				<div class="tinder--buttons flex justify-around mt-4 w-full max-w-md mx-auto">
+					<button @click="swipeLeft" class="w-16 h-16 bg-red-500 rounded-full shadow-lg flex items-center justify-center text-2xl text-white">
+						<i class="fas fa-times"></i>
+					</button>
+					<button @click="swipeRight" class="w-16 h-16 bg-green-500 rounded-full shadow-lg flex items-center justify-center text-2xl text-white">
+						<i class="fas fa-heart"></i>
+					</button>
+				</div>
+			</div>
 
-      <!-- SearchForm Positionné à droite -->
-      <div class="absolute top-0 right-20 mt-2 mr-2">
-        <SearchForm @submitForm="handleSearchCriteria" />
-			<info :profile="profiles.length ? profiles[profiles.length - 1] : {}" />
-			<!-- Check si le profil n'est pas vide avant d'essayer de l'ouvir -->
-      </div>
-      <signaler @report="handleReport" />
-    </div>
+			<!-- SearchForm Positionné à droite -->
+			<div class="absolute top-0 right-20 mt-2 mr-2">
+				<SearchForm @submitForm="handleSearchCriteria" />
+				<info :profile="profiles.length ? profiles[profiles.length - 1] : {}" />
+				<!-- Check si le profil n'est pas vide avant d'essayer de l'ouvir -->
+			</div>
+			<signaler @report="handleReport" />
+		</div>
 
-    <!-- Animation de match -->
-    <div
-      v-if="showAnimation"
-      class="fixed inset-0 flex items-center justify-center z-50 bg-white bg-opacity-20"
-    >
-      <LottieAnimation
-        :animationData="animationData"
-        :loop="false"
-        :autoplay="true"
-      />
-    </div>
-  </div>
+		<!-- Animation de match -->
+		<div v-if="showAnimation" class="fixed inset-0 flex items-center justify-center z-50 bg-white bg-opacity-20">
+			<LottieAnimation :animationData="animationData" :loop="false" :autoplay="true" />
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -144,181 +97,173 @@ const isSidebarOpen = ref(false);
 const hasReported = ref(false);
 
 const likeProfile = async (likedUserId, status) => {
-  try {
-    const response = await axios.post(
-      "http://localhost:3005/api/swipe/like_profil",
-      {
-        likedUserId,
-        status,
-      },
-      {
-        withCredentials: true,
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error liking/disliking profile:", error);
-  }
+	try {
+		const response = await axios.post(
+			"http://localhost:3005/api/swipe/like_profil",
+			{
+				likedUserId,
+				status,
+			},
+			{
+				withCredentials: true,
+			}
+		);
+		return response.data;
+	} catch (error) {
+		console.error("Error liking/disliking profile:", error);
+	}
 };
 
 const swipeLeft = async () => {
-  if (isSwiping || profiles.value.length === 0) return;
-  isSwiping = true; //
-  const profile_id = profiles.value[profiles.value.length - 1].id;
-  await likeProfile(profile_id, "dislike");
-  profiles.value.pop(); // Supprime la carte du haut
-  isSwiping = false;
+	if (isSwiping || profiles.value.length === 0) return;
+	isSwiping = true; //
+	const profile_id = profiles.value[profiles.value.length - 1].id;
+	await likeProfile(profile_id, "dislike");
+	profiles.value.pop(); // Supprime la carte du haut
+	isSwiping = false;
 };
 
 let isSwiping = false;
 
 const swipeRight = async () => {
-  if (isSwiping || profiles.value.length === 0) return;
-  isSwiping = true;
+	if (isSwiping || profiles.value.length === 0) return;
+	isSwiping = true;
 
-  const profile_id = profiles.value[profiles.value.length - 1].id;
-  const response = await likeProfile(profile_id, "like");
+	const profile_id = profiles.value[profiles.value.length - 1].id;
+	const response = await likeProfile(profile_id, "like");
 
-  if (response.match === true) {
-    showAnimation.value = true; // Déclenche l'animation
-    setTimeout(() => {
-      showAnimation.value = false; // Cache l'animation après 3 secondes
-    }, 3000);
-    console.log("Matched");
-  }
+	if (response.match === true) {
+		showAnimation.value = true; // Déclenche l'animation
+		setTimeout(() => {
+			showAnimation.value = false; // Cache l'animation après 3 secondes
+		}, 3000);
+		console.log("Matched");
+	}
 
-  profiles.value.pop(); // Supprime la carte du haut
-  isSwiping = false;
+	profiles.value.pop(); // Supprime la carte du haut
+	isSwiping = false;
 };
 
 const fetchProfileData = async () => {
-  try {
-    console.log("Je suis là");
-    console.log("Search criteria:", searchCriteria.value);
-    const response = await axios.get(
-      "http://localhost:3005/api/swipe/profil_to_match",
-      {
-        params: searchCriteria.value,
-        withCredentials: true,
-      }
-    );
+	try {
+		console.log("Je suis là");
+		console.log("Search criteria:", searchCriteria.value);
+		const response = await axios.get("http://localhost:3005/api/swipe/profil_to_match", {
+			params: searchCriteria.value,
+			withCredentials: true,
+		});
 
-    const { data } = response;
-    console.log("Données reçues:", data);
-    profiles.value = data.profiles.map((profile) => {
-      if (typeof profile.interests === "string") {
-        profile.interests = JSON.parse(profile.interests);
-      }
-      profile.photos = Array.isArray(profile.photos)
-        ? profile.photos.map((photo) => photo.replace("/app", ""))
-        : []; // Supprime app dans toutes les URL des photos
-      return profile;
-    });
-  } catch (error) {
-    console.error("Error fetching profiles:", error);
-  }
+		const { data } = response;
+		console.log("Données reçues:", data);
+		profiles.value = data.profiles.map((profile) => {
+			if (typeof profile.interests === "string") {
+				profile.interests = JSON.parse(profile.interests);
+			}
+			profile.photos = Array.isArray(profile.photos) ? profile.photos.map((photo) => photo.replace("/app", "")) : []; // Supprime app dans toutes les URL des photos
+			return profile;
+		});
+	} catch (error) {
+		console.error("Error fetching profiles:", error);
+	}
 };
 
 const handleReport = async () => {
-  hasReported.value = true;
-  const profile_id = profiles.value[profiles.value.length - 1].id;
-  console.log(
-    "Name of the profile to report:",
-    profiles.value[profiles.value.length - 1].firstname
-  );
-  await likeProfile(profile_id, "dislike");
-  profiles.value.pop();
-  setTimeout(() => {
-    hasReported.value = false;
-  }, 1000);
+	hasReported.value = true;
+	const profile_id = profiles.value[profiles.value.length - 1].id;
+	console.log("Name of the profile to report:", profiles.value[profiles.value.length - 1].firstname);
+	await likeProfile(profile_id, "dislike");
+	profiles.value.pop();
+	setTimeout(() => {
+		hasReported.value = false;
+	}, 1000);
 
-  try {
-    const response = await axios.post(
-      "http://localhost:3005/api/swipe/report_fake_profil",
-      {
-        profile_id,
-      },
-      {
-        withCredentials: true,
-      }
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error reporting profile:", error);
-  }
+	try {
+		const response = await axios.post(
+			"http://localhost:3005/api/swipe/report_fake_profil",
+			{
+				profile_id,
+			},
+			{
+				withCredentials: true,
+			}
+		);
+		return response.data;
+	} catch (error) {
+		console.error("Error reporting profile:", error);
+	}
 };
 
 const handleSearchCriteria = (criteria) => {
-  searchCriteria.value = criteria;
-  localStorage.setItem("searchCriteria", JSON.stringify(criteria));
+	searchCriteria.value = criteria;
+	localStorage.setItem("searchCriteria", JSON.stringify(criteria));
 
-  console.log("Search criteria received from child:", criteria);
-  window.location.reload();
+	console.log("Search criteria received from child:", criteria);
+	window.location.reload();
 };
 
 onMounted(() => {
-  const savedCriteria = localStorage.getItem("searchCriteria");
+	const savedCriteria = localStorage.getItem("searchCriteria");
 
-  if (savedCriteria) {
-    searchCriteria.value = JSON.parse(savedCriteria);
-    console.log("Restored search criteria:", searchCriteria.value);
-  }
-  console.log("Search form component:", searchCriteria.interests);
-  fetchProfileData();
+	if (savedCriteria) {
+		searchCriteria.value = JSON.parse(savedCriteria);
+		console.log("Restored search criteria:", searchCriteria.value);
+	}
+	console.log("Search form component:", searchCriteria.interests);
+	fetchProfileData();
 });
 </script>
 
 <style scoped>
 .card-stack {
-  perspective: 1000px;
+	perspective: 1000px;
 }
 
 .card {
-  position: absolute;
-  width: 100%;
-  transform-origin: bottom center;
-  transition: transform 0.5s ease-out, opacity 0.5s ease-out;
-  backface-visibility: hidden;
+	position: absolute;
+	width: 100%;
+	transform-origin: bottom center;
+	transition: transform 0.5s ease-out, opacity 0.5s ease-out;
+	backface-visibility: hidden;
 }
 
 .card.top-card {
-  z-index: 10;
+	z-index: 10;
 }
 
 .card:not(.top-card) {
-  transform: scale(0.9) translateY(20px);
+	transform: scale(0.9) translateY(20px);
 }
 
 .card.top-card.swiping {
-  opacity: 0;
-  transform: rotate(15deg) translateX(300px) translateY(-50px);
+	opacity: 0;
+	transform: rotate(15deg) translateX(300px) translateY(-50px);
 }
 
 .card.top-card.swiping-left {
-  opacity: 0;
-  transform: rotate(-15deg) translateX(-300px) translateY(-50px);
+	opacity: 0;
+	transform: rotate(-15deg) translateX(-300px) translateY(-50px);
 }
 
 .tinder--buttons {
-  position: relative;
-  z-index: 10;
-  margin-top: 20px; /* Espacement par rapport à la carte */
-  display: flex;
-  justify-content: center; /* Centrer les boutons horizontalement */
+	position: relative;
+	z-index: 10;
+	margin-top: 20px; /* Espacement par rapport à la carte */
+	display: flex;
+	justify-content: center; /* Centrer les boutons horizontalement */
 }
 .sidebar {
-  position: fixed;
-  top: 0;
-  right: -100%; /* Le panneau est initialement hors de la vue */
-  width: 300px; /* Largeur du panneau */
-  height: 100%;
-  background-color: white;
-  z-index: 50; /* Assure que le panneau est au-dessus du contenu */
-  transition: right 0.3s ease; /* Effet de transition lors de l'ouverture et de la fermeture */
-  box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5); /* Ombre pour l'effet de profondeur */
+	position: fixed;
+	top: 0;
+	right: -100%; /* Le panneau est initialement hors de la vue */
+	width: 300px; /* Largeur du panneau */
+	height: 100%;
+	background-color: white;
+	z-index: 50; /* Assure que le panneau est au-dessus du contenu */
+	transition: right 0.3s ease; /* Effet de transition lors de l'ouverture et de la fermeture */
+	box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5); /* Ombre pour l'effet de profondeur */
 }
 
 .sidebar.open {
-  right: 0; /* Le panneau est visible lorsqu'il est ouvert */
+	right: 0; /* Le panneau est visible lorsqu'il est ouvert */
 }
 </style>
--- front-end/components/swipe/reglage_swipe.vue
+++ front-end/components/swipe/reglage_swipe.vue
@@ -1,106 +1,92 @@
 <template>
-  <div class="relative max-w-sm mx-auto p-2">
-    <!-- Icône de réglage -->
-    <div class="flex justify-end mb-4">
-      <button @click="toggleSettings" class="p-2">
-        <img src="/img/reglages.png" alt="Settings" class="w-6 h-6" />
-      </button>
-    </div>
+	<div class="relative max-w-sm mx-auto p-2">
+		<!-- Icône de réglage -->
+		<div class="flex justify-end mb-4">
+			<button @click="toggleSettings" class="p-2">
+				<img src="/img/reglages.png" alt="Settings" class="w-6 h-6" />
+			</button>
+		</div>
 
-    <!-- Formulaire de critères de recherche, caché par défaut -->
-    <div v-if="showSettings" class="bg-white p-4 rounded-lg shadow-md">
-      <!-- Carousel pour les intérêts -->
-      <div class="mb-3">
-        <h3 class="text-md font-semibold">Select Interests</h3>
-        <Interests_select :selectedInterests="selectedInterests" @updateInterests="updateInterests" />
-      </div>
+		<!-- Formulaire de critères de recherche, caché par défaut -->
+		<div v-if="showSettings" class="bg-white p-4 rounded-lg shadow-md">
+			<!-- Carousel pour les intérêts -->
+			<div class="mb-3">
+				<h3 class="text-md font-semibold">Select Interests</h3>
+				<Interests_select :selectedInterests="selectedInterests" @updateInterests="updateInterests" />
+			</div>
 
-      <!-- Curseur d'écart d'âge -->
-      <div class="mb-3">
-        <h3 class="text-md font-semibold">Select Age Range</h3>
-        <div class="flex space-x-2 items-center">
-          <input 
-            type="number" 
-            v-model.number="age_min" 
-            min="18" 
-            max="100" 
-            placeholder="Min age" 
-            class="w-1/2 p-2 border rounded"
-          />
-          <span>-</span>
-          <input 
-            type="number" 
-            v-model.number="age_max" 
-            min="18" 
-            max="100" 
-            placeholder="Max age" 
-            class="w-1/2 p-2 border rounded"
-          />
-        </div>
-        <p v-if="age_min > age_max" class="text-red-500 text-sm mt-2">Minimum age must be less than maximum age.</p>
-      </div>
+			<!-- Curseur d'écart d'âge -->
+			<div class="mb-3">
+				<h3 class="text-md font-semibold">Select Age Range</h3>
+				<div class="flex space-x-2 items-center">
+					<input type="number" v-model.number="age_min" min="18" max="100" placeholder="Min age" class="w-1/2 p-2 border rounded" />
+					<span>-</span>
+					<input type="number" v-model.number="age_max" min="18" max="100" placeholder="Max age" class="w-1/2 p-2 border rounded" />
+				</div>
+				<p v-if="age_min > age_max" class="text-red-500 text-sm mt-2">Minimum age must be less than maximum age.</p>
+			</div>
 
-      <!-- Sélecteur d'écart de "fame rating" -->
-      <div class="mb-3">
-        <h3 class="text-md font-semibold">Select Fame Rating Gap</h3>
-        <select v-model="fame_rating_gap" class="w-full p-2 border rounded text-sm">
-          <option value="">Select Fame Rating Gap</option>
-          <option value="1-10">1-10</option>
-          <option value="10-30">10-30</option>
-          <option value="30-50">30-50</option>
-          <option value="50-100">50-100</option>
-        </select>
-      </div>
+			<!-- Sélecteur d'écart de "fame rating" -->
+			<div class="mb-3">
+				<h3 class="text-md font-semibold">Select Fame Rating Gap</h3>
+				<select v-model="fame_rating_gap" class="w-full p-2 border rounded text-sm">
+					<option value="">Select Fame Rating Gap</option>
+					<option value="1-10">1-10</option>
+					<option value="10-30">10-30</option>
+					<option value="30-50">30-50</option>
+					<option value="50-100">50-100</option>
+				</select>
+			</div>
 
-      <!-- Formulaire pour la localisation -->
-      <div class="mb-3">
-        <h3 class="text-md font-semibold">Select a city </h3>
-        <select v-model="location_gap" class="w-full p-2 border rounded text-sm">
-          <option value="">Select your city </option>
-          <option value="Paris">Paris</option>
-          <option value="London">London</option>
-          <option value="Toronto">Toronto</option>
-          <option value="Casablanca">Casablanca</option>
-        </select>
-      </div>
+			<!-- Formulaire pour la localisation -->
+			<div class="mb-3">
+				<h3 class="text-md font-semibold">Select a city</h3>
+				<select v-model="location_gap" class="w-full p-2 border rounded text-sm">
+					<option value="">Select your city</option>
+					<option value="Paris">Paris</option>
+					<option value="London">London</option>
+					<option value="Toronto">Toronto</option>
+					<option value="Casablanca">Casablanca</option>
+				</select>
+			</div>
 
-      <!-- Bouton de soumission -->
-      <div class="text-center">
-        <button @click="submitForm" class="bg-blue-500 text-white px-3 py-2 rounded text-sm">Start Search</button>
-      </div>
-    </div>
-  </div>
+			<!-- Bouton de soumission -->
+			<div class="text-center">
+				<button @click="submitForm" class="bg-blue-500 text-white px-3 py-2 rounded text-sm">Start Search</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import Interests_select from '~/components/profil/interests.vue';
+import { ref } from "vue";
+import Interests_select from "~/components/profil/interests.vue";
 
-const emit = defineEmits(['submitForm']); // Utilise defineEmits pour déclarer l'événement
+const emit = defineEmits(["submitForm"]); // Utilise defineEmits pour déclarer l'événement
 
 const selectedInterests = ref([]); // Déclare selectedInterests ici
-const age_min = ref('');
-const age_max = ref('');
-const location_gap = ref('');
-const fame_rating_gap = ref('');
+const age_min = ref("");
+const age_max = ref("");
+const location_gap = ref("");
+const fame_rating_gap = ref("");
 const showSettings = ref(false);
 
 const updateInterests = (newInterests) => {
-  selectedInterests.value = newInterests;
+	selectedInterests.value = newInterests;
 };
 
 const toggleSettings = () => {
-  showSettings.value = !showSettings.value;
+	showSettings.value = !showSettings.value;
 };
 
 const submitForm = () => {
-  const searchCriteria = {
-    interests: selectedInterests.value,
-    age_gap: `${age_min.value}-${age_max.value}`,
-    location_gap: location_gap.value,
-    fame_rating_gap: fame_rating_gap.value,
-  };
-  emit('submitForm', searchCriteria);
+	const searchCriteria = {
+		interests: selectedInterests.value,
+		age_gap: `${age_min.value}-${age_max.value}`,
+		location_gap: location_gap.value,
+		fame_rating_gap: fame_rating_gap.value,
+	};
+	emit("submitForm", searchCriteria);
 };
 </script>
 
