6cb3b3443977ee4015403d181ad30fa8cb38099e
root <mohammed-981@hotmail.fr>
2025-01-28 18:34:58 +0100

    Remove import of the components

--- front-end/pages/register.vue
+++ front-end/pages/register.vue
@@ -1,9 +1,7 @@
 <template>
-	<div>
-		<RegisterForm />
-	</div>
+  <div>
+    <RegisterForm />
+  </div>
 </template>
 
-<script setup>
-import RegisterForm from "~/components/RegisterForm.vue";
-</script>
+<script setup></script>
